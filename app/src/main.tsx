73bbac791add8096f933542a5f84470dbec71aa0
hasakey <1341328435@qq.com>
2026-06-25 01:55:14 +0800

    feat(app): 启动注入 repo + 加载(前端地基完成)

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { ThemeProvider } from './themes/ThemeProvider'
+import { createRepository } from './db/repositoryFactory'
+import { useTaskStore } from './store/taskStore'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+async function bootstrap() {
+  const repo = await createRepository()
+  useTaskStore.getState().reset(repo)
+  await useTaskStore.getState().loadFromRepo()
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>,
+  )
+}
+
+bootstrap()
